a6b34183976a417607b1f68d9f6550d756c689fc
Andrii Bezus <silverbird5567@gmail.com>
2025-11-13 10:11:05 +0200

    fix4

--- project/settings.py
+++ project/settings.py
@@ -74,7 +74,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'Task_17_Basic_Weather_Reminder.urls'
+ROOT_URLCONF = 'project.urls'
 
 TEMPLATES = [
     {
@@ -92,7 +92,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'Task_17_Basic_Weather_Reminder.wsgi.application'
+WSGI_APPLICATION = 'project.wsgi.application'
 
 
 # Database
